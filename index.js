926585990554d1b53bd9fe6d64f4a1045f51b92e
Cody Regis <codyregis6891@yahoo.com>
2021-03-06 22:21:41 -0800

    added index.js, started writing inquirer function

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+inquirer
+    .prompt([
+        {
+            name: 'title',
+            message: 'What would you like to title your README?',
+            type: 'input',
+        },
+        {
+            name: 'description',
+            message: 'Add a description of your project.',
+            type: 'input',
+        },
+        {
+            name: 'install',
+            message: 'Add any installation instructions.',
+            type: 'input',
+        },
+        {
+            
+        }
+    ])
