206557e31f7f0406e9dbc5ac3b25e213ea0b7e18
Cody Regis <codyregis6891@yahoo.com>
2021-03-06 23:57:46 -0800

    finished inquirer function

--- index.js
+++ index.js
@@ -19,6 +19,35 @@ inquirer
             type: 'input',
         },
         {
-            
+            name: 'usage',
+            message: 'Add usage information.',
+            type: 'input',
+        },
+        {
+            name: 'contribution',
+            message: 'Add contribution guidelines.',
+            type: 'input',
+        },
+        {
+            name: 'test',
+            message: 'Add test instructions.',
+            type: 'input',
+        },
+        {
+            name: 'license',
+            message: 'Would you like to add a license for your application?',
+            type: 'list',
+            choices: ['None', 'Apache', 'MIT', 'BSD'],
+        },
+        {
+            name: 'github',
+            message: 'Enter your Github username.',
+            type: 'input',
+        },
+        {
+            name: 'email',
+            message: 'Add your email address.',
+            type: 'input',
         }
     ])
+    .then
