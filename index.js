41fd38e88d255636a83b63abc67514290dcc643e
Cody Regis <codyregis6891@yahoo.com>
2021-03-09 22:26:42 -0800

    license badge function added

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ inquirer
             name: 'license',
             message: 'Would you like to add a license for your application?',
             type: 'list',
-            choices: ['None', 'Apache', 'MIT', 'BSD'],
+            choices: ['None', 'Apache', 'MIT', 'GPL', 'ISC'],
         },
         {
             name: 'github',
@@ -56,60 +56,92 @@ inquirer
         err ? console.log(err) : console.log('Success!'));
     });
 
-const generateREADME = (response) =>
+const generateLicenseBadge = (license) => {
+    let licenseBadge;
+    switch(license) {
+        case "Apache":
+            licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+            return licenseBadge;
+        case "MIT":
+            licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+            return licenseBadge;
+        case "GPL":
+            licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+            return licenseBadge;
+        case "ISC":
+            licenseBadge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+            return licenseBadge;
+        default:
+            licenseBadge = "";
+            return licenseBadge;
+    };
+};
+
+const generateREADME = (response) => {
+
+    const badge = generateLicenseBadge(response.license);
+
+    // console.log(badge);
+
+    const textLayout = 
     `# ${response.title}
 
-    ## Table of Contents
-
-    * [Description](#description)
-
-    * [Installation](#installation)
-
-    * [Usage](#usage)
-
-    * [License](#license)
-
-    * [Contributing](#contributing)
-
-    * [Tests](#tests)
-
-    * [Questions](#questions)
+${badge}
 
 
-    ## Description
+## Table of Contents
 
-    ${response.description}
+* [Description](#description)
 
+* [Installation](#installation)
 
-    ## Installation
+* [Usage](#usage)
 
-    ${response.install}
+* [License](#license)
 
+* [Contributing](#contributing)
 
-    ## Usage
+* [Tests](#tests)
 
-    ${response.usage}
-
-
-    ## License 
-
-    This application has the following License: ${response.license}
+* [Questions](#questions)
 
 
-    ## Contributing
+## Description
 
-    ${response.contribution}
-
-
-    ## Tests
-
-    ${response.test}
+${response.description}
 
 
-    ## Questions
+## Installation
 
-    [Check out my Github profile!](https://www.github.com/${response.github})
+${response.install}
 
-    Have any further questions? Don't hesitate to contact me via email:
-    * ${response.email}`;
-    
+
+## Usage
+
+${response.usage}
+
+
+## License 
+
+This application has the following License: ${response.license}
+
+
+## Contributing
+
+${response.contribution}
+
+
+## Tests
+
+${response.test}
+
+
+## Questions
+
+[Check out my Github profile!](https://www.github.com/${response.github})
+
+Have any further questions? Don't hesitate to contact me via email:
+* ${response.email}`;
+
+    return textLayout;
+};
